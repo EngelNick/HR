85c578af953c7232cf5a537fbfbb52e109a074ba
EngelNick <zsnikolay@gmail.com>
2017-09-20 16:48:28 +0300

    index.js error

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(cors({
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/pulic'));
+app.use(express.static(__dirname + '/public'));
 app.use('/vacancies', vacancies );
 app.use('/savenews', savenews );
 app.use('/articles', articles );
